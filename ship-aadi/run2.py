674f4787f694e42164bf09df139a1d515ca48161
GameGenius101 <166352462+GameGenius101@users.noreply.github.com>
2025-09-01 18:05:32 -0700

    ship code by aadi

---
+++ ship-aadi/run2.py
@@ -0,0 +1,15 @@
+from movement import move, turn, move_backwards
+
+def run():
+    print("Running Mission 2...")
+    turn(84)
+    move(17)
+    turn(10)
+    move(0.6)
+    turn(15)
+    move_backwards(5)
+    turn(-28)
+    move(10)
+    move_backwards(2)
+    turn(-40)
+    move(30)
