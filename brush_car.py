90b69f7af0eddcaa439cba284d9d3a4f87b9c62d
vivaankum <vvkumawat09@gmail.com>
2025-09-23 19:25:22 -0700

    brush_car.py
    added the car part

---
+++ brush_car.py
@@ -0,0 +1,17 @@
+from movement import *
+
+def run():
+    print("Running Mission 1...")
+    turn(10.75)
+    move(23)
+    turn(-130)
+    turn(177)
+    move(8)
+    turn(180)
+    motor(1, 500, 1, "d")
+    move_backwards(4)
+    motor(-1, 500, 1, "d")
+    motor(1, 500, 1, "d")
+    move(8)
+    turn(-45)
+    move(23)
--- redstickthing.py
+++
@@ -1,9 +0,0 @@
-from movement import *
-
-def run():
-    print("Running Mission 1...")
-    turn(10)
-    move(23)
-    turn(-130)
-    turn(140)
-    move_backwards(23)
