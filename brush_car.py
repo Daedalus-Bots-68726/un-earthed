c5e551e895794d4437271a0706849f251321c0cd
vivaankum <vvkumawat09@gmail.com>
2025-09-27 15:16:21 -0700

    brush_car.py change to make more efficient

--- brush_car.py
+++ brush_car.py
@@ -1,7 +1,7 @@
 from movement import *
 
 def run():
-    print("Running Mission 1***")
+    print("Running Mission 1...")
     turn(10.75)
     move(23)
     turn(-130)
@@ -10,8 +10,8 @@ def run():
     turn(180)
     motor(1, 500, 1, "d")
     move_backwards(4)
+    turn(-3)
     motor(-1, 500, 1, "d")
-    motor(1, 500, 1, "d")
     move(8)
     turn(-45)
     move(23)
